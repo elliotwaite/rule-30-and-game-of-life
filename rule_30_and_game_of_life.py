73fc4e27163015fa6dc7cae28ec13995a0952ba3
elliotwaite <1767836+elliotwaite@users.noreply.github.com>
2020-02-12 14:45:25 -0800

    Add option to change rule

--- rule_30_and_game_of_life.py
+++ rule_30_and_game_of_life.py
@@ -29,12 +29,38 @@ SECS = int(60 * 3.5)  # 3 mins 30 secs.
 PIXEL_SIZE = 6
 OUTPUT_PATH = 'videos/youtube-3m-30s-6px.mp4'
 
-FPS = 30  # Frames per second.
+# YouTube channel art.
+# VIDEO_WIDTH = 2880
+# VIDEO_HEIGHT = 1800
+# SECS = 60
+# PIXEL_SIZE = 4
+# OUTPUT_PATH = 'videos/youtube-channel-art-2.mp4'
+
+FPS = 60  # Frames per second.
 HIGH_QUALITY = True
 
 STATE_WIDTH = VIDEO_WIDTH // PIXEL_SIZE
 STATE_HEIGHT = VIDEO_HEIGHT // PIXEL_SIZE
 NUM_FRAMES = SECS * FPS
+
+# `RULE` specifies which cellular automaton rule to use.
+RULE = 30
+
+# `X_OFFSET` specifies how far from the center to place the initial first pixel.
+X_OFFSET = 0
+
+# These settings can be used for rule 110, which only grows to the left, so we
+# offset the starting pixel to be close to the right edge of the screen.
+# RULE = 110
+# X_OFFSET = VIDEO_WIDTH // PIXEL_SIZE // 2 - 1 - 60 * 4
+
+# The Game of Life state wraps across the left and right edges of the state,
+# and dies out at the top of the state (all values of the top row are zero).
+# By adding padding to the state, you extend the state beyond the edges of the
+# visible window, essentially hiding the wrapping and/or dying out aspects of
+# the state.
+GOL_STATE_WIDTH_PADDING = VIDEO_WIDTH
+GOL_STATE_HEIGHT_PADDING = VIDEO_HEIGHT
 
 
 class Rule30AndGameOfLife:
@@ -45,16 +71,20 @@ class Rule30AndGameOfLife:
     self.height = height
 
     self.gol_height = int(height * gol_percentage)
-    self.gol_state = np.zeros((self.height, self.width), np.uint8)
+    self.gol_state_width = self.width + GOL_STATE_WIDTH_PADDING * 2
+    self.gol_state_height = self.gol_height + GOL_STATE_HEIGHT_PADDING
+
+    self.gol_state = np.zeros((self.gol_state_height, self.gol_state_width),
+                              np.uint8)
 
     self.row_padding = num_frames // 2
-    self.row_width = self.width + self.row_padding * 2
+    self.row_width = self.gol_state_width + self.row_padding * 2
     self.row = np.zeros(self.row_width, np.uint8)
-    self.row[self.row_width // 2] = 1
+    self.row[self.row_width // 2 + X_OFFSET] = 1
 
     self.rows_height = self.height - self.gol_height
     self.rows = np.concatenate((
-        np.zeros((self.rows_height - 1, self.width), np.uint8),
+        np.zeros((self.rows_height - 1, self.gol_state_width), np.uint8),
         self.row[None, self.row_padding:-self.row_padding]
     ))
 
@@ -62,7 +92,7 @@ class Rule30AndGameOfLife:
     self.gol_neighbors = np.array([[1, 1, 1],
                                    [1, 0, 1],
                                    [1, 1, 1]], dtype=np.uint8)
-    self.rule = 30
+    self.rule = RULE
     self.rule_kernel = None
     self.update_rule_kernel()
 
@@ -103,7 +133,7 @@ class Rule30AndGameOfLife:
                                 np.uint8)
 
   def update_rows_and_gol_state(self):
-    # Update rows.
+    # Update `rows` (the state of the 2D cellular automaton).
     rule_index = signal.convolve2d(self.row[None, :],
                                    self.row_neighbors[None, :],
                                    mode='same', boundary='wrap')
@@ -114,7 +144,7 @@ class Rule30AndGameOfLife:
         self.row[None, self.row_padding:-self.row_padding]
     ))
 
-    # Update gol_state.
+    # Update `gol_state` (the state of the 3D cellular automaton).
     num_neighbors = signal.convolve2d(self.gol_state, self.gol_neighbors,
                                       mode='same', boundary='wrap')
     self.gol_state = np.logical_or(num_neighbors == 3,
@@ -123,17 +153,20 @@ class Rule30AndGameOfLife:
                                    ).astype(np.uint8)
 
     self.gol_state = np.concatenate((
-        np.zeros((1, self.width), np.uint8),
+        np.zeros((1, self.gol_state_width), np.uint8),
         self.gol_state[1:-1],
         transfer_row
     ))
 
   def update_decay(self):
-    full_state = np.concatenate(
-        (self.gol_state[-self.gol_height:], self.rows), axis=0)
+    visible_state = np.concatenate(
+        (self.gol_state[-self.gol_height:,
+         GOL_STATE_WIDTH_PADDING:-GOL_STATE_WIDTH_PADDING],
+         self.rows[:, GOL_STATE_WIDTH_PADDING:-GOL_STATE_WIDTH_PADDING]),
+        axis=0)
     self.decay += 1
     self.decay = np.clip(self.decay, None, len(self.colors) - 1)
-    self.decay *= 1 - full_state
+    self.decay *= 1 - visible_state
 
   def update_rgb(self):
     self.rgb = self.colors[self.decay]
